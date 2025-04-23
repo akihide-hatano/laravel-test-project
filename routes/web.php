ed7cc18830308df924e42dc0af8465aa51941bb4
akihide-hatano <a.h.19841202@gmail.com>
2025-04-24 08:24:05 +0900

    post画面を作成しました。

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,5 +21,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('post/create',[PostController::class,'create']);
 
 require __DIR__.'/auth.php';
